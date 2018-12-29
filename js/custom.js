852d8d956e4f1b749ce6da77c4101a4546cc9b1f
Kyle Hughes <blinded93@gmail.com>
2018-12-29 13:58:51 -0800

    update blog style

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$(function() {
+ cursor();
+});
+
+function cursor() {
+  $("#cursor").each(function() {
+    var elem = $(this);
+    setInterval(function() {
+        if (elem.css('visibility') == 'hidden') {
+            elem.css('visibility', 'visible');
+        } else {
+            elem.css('visibility', 'hidden');
+        }
+    }, 700);
+  });
+}
